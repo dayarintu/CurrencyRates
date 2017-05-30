bc527baea3311681f2f31c62637d55ea1e22e8cb
Daya <daya@Dayas-MacBook-Air.local>
2017-05-30 19:51:40 -0400

    code changes in implementation

--- Currency Rates/Currency Rates/ViewController.swift
+++ Currency Rates/Currency Rates/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var AustralianCurrency = 0
+    var IndianCurrency = 0
+    var BritanCurrency = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://api.fixer.io/latest")
+        let task = URLSession.shared.dataTask(with: url!) {
+            (data, response , error) in
+            if error != nil {
+                print("Error")
+            } else {
+                if let content = data
+                {
+                    do {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        if let rates = myJson["rates"] as? NSDictionary
+                        {
+                            if let AustralianCurrency = rates["AUD"] ,
+                                let BritanCurrency = rates["GBP"] ,
+                                let IndianCurrency = rates["INR"]
+                            {
+                                print(IndianCurrency)
+                                print(BritanCurrency)
+                                print(AustralianCurrency)
+                            }
+                        }
+                    }
+                    catch {
+                        
+                    }
+                }
+            }
+            
+        }
+        
+        task.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
